7da64348a21a7034d76e946cd886c02f6d951ed6
AGonzalez1125 <andre.g1125@icloud.com>
2024-09-15 19:53:27 -0400

    Update Module3.R

--- Module3.R
+++ Module3.R
@@ -2,7 +2,7 @@
 ##Assignment: Module 3
 
 
-
+##Original provided in assignment:
 #Name <- c("Jeb", “Donald”, "Ted”, “Marco” “Carly”, “Hillary”, “Berine”)
 #ABC political poll results <- c(4, 62 51, 21, 2, 14, 15)
 #CBS political poll results <- c(12, 75, 43, 19, 1, 21, 19)
