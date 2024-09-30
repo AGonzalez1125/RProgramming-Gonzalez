88e3da341d421bcf23b685ef93a53595900e3579
AGonzalez1125 <andre.g1125@icloud.com>
2024-09-29 22:42:18 -0400

    Update Module5.R

--- Module5.R
+++ Module5.R
@@ -12,3 +12,10 @@ B <- matrix(1 : 1000, nrow = 10)
 ## Creating the inverse of the matrix
 a <- solve(A)
 b <- solve(B)
+
+## This creates errors as the A matrix is linearly dependant due to the sequence and B matrix is not a square matrix so it cannot be inverted
+Aa <- matrix(runif(100), nrow = 10)
+Aa
+## Invert and display
+aa <- solve(Aa)
+aa
