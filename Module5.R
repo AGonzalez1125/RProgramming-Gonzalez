b106e8f721b078fcc174a05ea1e83349ec2df62c
AGonzalez1125 <andre.g1125@icloud.com>
2024-09-29 22:39:36 -0400

    Create Module5.R

---
+++ Module5.R
@@ -0,0 +1,14 @@
+## Assignment: Module 5
+## Programmer: Andre Gonzalez
+
+
+
+## Create Sample Matrices to perform math
+
+A <- matrix(1 : 100, nrow = 10 , ncol = 10)
+B <- matrix(1 : 1000, nrow = 10)
+
+
+## Creating the inverse of the matrix
+a <- solve(A)
+b <- solve(B)
