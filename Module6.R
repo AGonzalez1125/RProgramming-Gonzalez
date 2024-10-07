fc5810a1d8659fb13c18c7937c032ece4f57c922
AGonzalez1125 <andre.g1125@icloud.com>
2024-10-06 23:00:31 -0400

    Create Module6.R

---
+++ Module6.R
@@ -0,0 +1,14 @@
+## Assignment: Module 5
+## Programmer: Andre Gonzalez
+
+
+## Create Matrices
+A <- matrix(c(2,0,1,3), ncol = 2)
+B <- matrix(c(5,2,4,-1), ncol=2)
+
+## Addition
+A + B
+
+## Subtraction
+A - B
+
