06476581a8aae414f6419133834a7e1a13e88369
AGonzalez1125 <andre.g1125@icloud.com>
2024-10-14 22:10:22 -0400

    Update Module7.R

--- Module7.R
+++ Module7.R
@@ -15,6 +15,16 @@ isS4(mtcars)
 
 ## Creating Example S3 object
 s <- list(name = "Myself", age = 29, GPA = 3.5)
+s
 
-## Creating S4 object,
+## Creating S4 class and object,
+setClass("student",
+         representation(
+           name="character",
+           age="numeric",
+           GPA="numeric")
+)
 s4 <- new("student",name="Myself", age=29, GPA=3.5)
+s4
+
+
